4388128981706077add20999e0d3e7b423f6df04
hninsatphyusin <hninsatphyusin99@gmail.com>
2025-10-16 17:44:42 +0800

    Add Predicate for Student in Class Group

---
+++ src/main/java/seedu/address/model/person/StudentInClassGroupPredicate.java
@@ -0,0 +1,42 @@
+package seedu.address.model.person;
+
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+import seedu.address.commons.util.ToStringBuilder;
+
+/**
+ * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
+ */
+public class StudentInClassGroupPredicate implements Predicate<Person> {
+    private final String classGroup;
+
+    public StudentInClassGroupPredicate(String classGroup) {
+        this.classGroup = classGroup;
+    }
+
+    @Override
+    public boolean test(Person person) {
+        return person.getClassGroups().contains(String classGroup);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof StudentInClassGroupPredicate)) {
+            return false;
+        }
+
+        StudentInClassGroupPredicate otherStudentInClassGroupPredicate = (StudentInClassGroupPredicate) other;
+        return classGroup.equals(otherStudentInClassGroupPredicate.classGroup);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).add("classGroup", classGroup).toString();
+    }
+}
