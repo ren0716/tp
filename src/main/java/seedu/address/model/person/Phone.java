8ba11de607f1765d0afacbd372107cbf141de6b0
Yu Tingan <163671620+llamawithhats@users.noreply.github.com>
2025-10-30 03:49:05 +0800

    edit phone regex to allow spaces and +, and >= 8 digits

--- src/main/java/seedu/address/model/person/Phone.java
+++ src/main/java/seedu/address/model/person/Phone.java
@@ -11,8 +11,8 @@ public class Phone {
 
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Phone number must be a number at least 3 digits long, and it should not be blank.";
-    public static final String VALIDATION_REGEX = "\\d{3,}";
+            "Phone number must be a number at least 8 digits long, and it should not be blank.";
+    public static final String VALIDATION_REGEX = "^(?=(?:.*\\d){8,})\\+?[\\d ]+$";
     public final String value;
 
     /**
